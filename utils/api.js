d574a2796feb7390854d8263b155bc026109d9f0
german.munshau <g.munshau@yandex.ru>
2024-08-06 12:28:16 +0600

    refactoring

--- utils/api.js
+++ utils/api.js
@@ -29,6 +29,7 @@ const postOptions = (data) => {
 }
 
 const updateToken = (token) => {
+    console.log('chatId', chatId)
     const data = JSON.parse(fs.readFileSync(DB, {encoding: 'utf8'}))
     data[chatId] = token
     fs.writeFileSync(DB, JSON.stringify(data, null, 2), {encoding: "utf8", flag: 'w',});
