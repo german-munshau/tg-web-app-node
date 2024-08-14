67ec821a3a4bc37e5b1b5aaa87ab1c247de80c8a
german.munshau <g.munshau@yandex.ru>
2024-08-14 20:38:20 +0600

    sendMessage endpoint

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,3 @@
-const ApiError = require('../error/ApiError');
-const {postOptions, getOptions, getNewToken} = require("../utils/api");
-
-const CLARIS_API_URL = process.env.CLARIS_API_URL
-const TELEGRAM_URL = process.env.TELEGRAM_URL
-const BOT_TOKEN = process.env.BOT_TOKEN
 const WEB_APP_URL = process.env.WEB_APP_URL
 
 class MessageController {
@@ -11,63 +5,29 @@ class MessageController {
     async get(req, res, next) {
 
         try {
-            const response = await bot.sendMessage(req.query.chat_id, req.query.text)
+            const {chat_id, text} = req.query
+            const id = req.params['id']
 
-            const webAppUrl = WEB_APP_URL + `/show/${req.params['id']}?chat_id=${req.query.chat_id}&message_id=${response.messageId}`
+            const response = await bot.sendMessage(chat_id, text)
+            const message_id = response.message_id
+
+            const webAppUrl = WEB_APP_URL + `/show/${id}?chat_id=${chat_id}&message_id=${message_id}`
 
             console.log('webAppUrl', webAppUrl)
 
             await bot.editMessageReplyMarkup({
                 inline_keyboard: [
-                    [{
-                        text: 'Открыть',
-                        web_app: {url: webAppUrl},
-                    }]
+                    [{text: 'Открыть', web_app: {url: webAppUrl}}]
                 ]
-            }, {
-                chat_id: req.query.chat_id,
-                message_id: response.message_id
-            })
+            }, {chat_id, message_id})
 
-            console.log(response)
+            console.log(response) // ответ от телеграмма
             return res.status(200).json(response)
 
         } catch (e) {
             console.log(e)
             return res.status(500).json({message: 'Error'})
         }
-
-
-        //     console.log('URL: ', req.originalUrl)
-        //
-        //     const url = `${CLARIS_API_URL}/vNext/v1/documents?filterBy=serialNumber=${req.query.serialNumber}`
-        //     let response = await fetch(url, getOptions(req.query.chat_id))
-        //
-        //     if (response.ok) {
-        //         console.log('status: OK')
-        //         const data = await response.json()
-        //         return res.status(200).json(data)
-        //     } else if (response.status === 401) {
-        //         console.log('status: 401')
-        //         const isNewToken = await getNewToken(req.query.chat_id)
-        //         if (isNewToken) {
-        //             console.log('Повторная попытка выгрузки документа')
-        //             let response = await fetch(url, getOptions(req.query.chat_id))
-        //             if (response.ok) {
-        //                 const data = await response.json()
-        //                 return res.status(200).json(data)
-        //             } else {
-        //                 return res.status(response.status).json({})
-        //             }
-        //         } else {
-        //             console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
-        //             return res.status(500).json({message: 'Не найдено инфо о пользователе в базе бота, необходима авторизация'})
-        //         }
-        //     }
-        //
-        // } catch (e) {
-        //     next(ApiError.badRequest(e.message))
-        // }
     }
 
 }
