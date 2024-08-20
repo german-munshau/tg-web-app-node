67e4719e0ce7a64119f10097c71dcf01ef1ab5fa
german.munshau <g.munshau@yandex.ru>
2024-08-20 12:45:41 +0600

    refactoring

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -49,7 +49,7 @@ class DocumentController {
 
     async getById(req, res, next) {
         console.log('URL: ', req.originalUrl)
-        const url = CLARIS_API_URL + '/vNext/v1/documents/' + req.params["id"]
+        const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}`
         let response = await fetch(url, getOptions(req.query.chat_id))
         if (response.status === 200) {
             const data = await response.json()
@@ -76,41 +76,6 @@ class DocumentController {
         return next(ApiError.badRequest('Документ не найден'))
     }
 
-    // async getById(req, res, next) {
-    //     try {
-    //         console.log('URL: ', req.originalUrl)
-    //         const url = CLARIS_API_URL + '/vNext/v1/documents/' + req.params["id"]
-    //         let response = await fetch(url, getOptions(req.query.chat_id))
-    //         if (response.ok) {
-    //             console.log('status: OK')
-    //             const data = await response.json()
-    //             return res.status(200).json(data)
-    //         } else if (response.status === 401) {
-    //             console.log('status: 401')
-    //             const isNewToken = await getNewToken(req.query.chat_id)
-    //             if (isNewToken) {
-    //                 console.log('Повторная попытка выгрузки документа')
-    //                 let response = await fetch(url, getOptions(req.query.chat_id))
-    //                 if (response.ok) {
-    //                     const data = await response.json()
-    //                     return res.status(200).json(data)
-    //                 } else {
-    //                     return res.status(response.status).json({})
-    //                 }
-    //             } else {
-    //                 console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
-    //                 return res.status(500).json({message: 'Необходима авторизация в системе Кларис'})
-    //             }
-    //         } else {
-    //             console.log('status:', response.status, response.statusText)
-    //             return res.status(response.status).json({})
-    //         }
-    //
-    //     } catch (e) {
-    //         next(ApiError.badRequest(e.message))
-    //     }
-    // }
-
     async agree(req, res, next) {
         try {
             const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}/agree`
@@ -118,14 +83,15 @@ class DocumentController {
             console.log('body: ', req.body)
             const {chatId, messageId, number, comment} = req.body
             const response = await fetch(url, postOptions(chatId, comment))
-            if (response.ok) {
-                await bot.editMessageText(`Документ № ${number} согласован`, {chat_id: chatId, message_id: messageId})
+            if (response.status === 200) {
                 console.log('Status:', response.status, response.statusText)
+                return await bot.editMessageText(`Документ № ${number} согласован`, {chat_id: chatId, message_id: messageId})
             }
-            return await res.status(response.status).json({})
+            return next(ApiError.badRequest('Ошибка при согласовании'))
+            // return await res.status(response.status).json({})
         } catch (e) {
             console.log(e)
-            next(ApiError.badRequest(e.message))
+            return next(ApiError.badRequest(e.message))
         }
     }
 
