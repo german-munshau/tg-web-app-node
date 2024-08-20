503016453f912f5411fdac4d7c1eb1e564fe4f8e
german.munshau <g.munshau@yandex.ru>
2024-08-20 12:23:05 +0600

    search doc

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -6,7 +6,6 @@ const CLARIS_API_URL = process.env.CLARIS_API_URL
 class DocumentController {
 
     async get(req, res, next) {
-
         console.log('URL: ', req.originalUrl)
         const url = `${CLARIS_API_URL}/vNext/v1/documents?filterBy=serialNumber=${req.query.serialNumber}`
         let response = await fetch(url, getOptions(req.query.chat_id))
@@ -47,40 +46,70 @@ class DocumentController {
         return next(ApiError.badRequest('Документ не найден'))
     }
 
+
     async getById(req, res, next) {
-        try {
-            console.log('URL: ', req.originalUrl)
-            const url = CLARIS_API_URL + '/vNext/v1/documents/' + req.params["id"]
-            let response = await fetch(url, getOptions(req.query.chat_id))
-            if (response.ok) {
-                console.log('status: OK')
-                const data = await response.json()
-                return res.status(200).json(data)
-            } else if (response.status === 401) {
-                console.log('status: 401')
-                const isNewToken = await getNewToken(req.query.chat_id)
-                if (isNewToken) {
-                    console.log('Повторная попытка выгрузки документа')
-                    let response = await fetch(url, getOptions(req.query.chat_id))
-                    if (response.ok) {
-                        const data = await response.json()
-                        return res.status(200).json(data)
-                    } else {
-                        return res.status(response.status).json({})
-                    }
+        console.log('URL: ', req.originalUrl)
+        const url = CLARIS_API_URL + '/vNext/v1/documents/' + req.params["id"]
+        let response = await fetch(url, getOptions(req.query.chat_id))
+        if (response.status === 200) {
+            const data = await response.json()
+            console.log('status: OK')
+            return res.status(200).json(data)
+        } else if (response.status === 401) {
+            console.log('status: 401 - Получение токена')
+            const isNewToken = await getNewToken(req.query.chat_id)
+            if (isNewToken) {
+                console.log('Повторная попытка выгрузки документа')
+                let response = await fetch(url, getOptions(req.query.chat_id))
+                if (response.status === 200) {
+                    const data = await response.json()
+                    return res.status(200).json(data)
                 } else {
-                    console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
-                    return res.status(500).json({message: 'Необходима авторизация в системе Кларис'})
+                    return next(ApiError.forbidden('Ошибка выгрузки документа'))
                 }
             } else {
-                console.log('status:', response.status, response.statusText)
-                return res.status(response.status).json({})
+                console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
+                return next(ApiError.forbidden('Необходима авторизация в системе Кларис'))
             }
-
-        } catch (e) {
-            next(ApiError.badRequest(e.message))
         }
+        console.log('status: 404 Документ не найден')
+        return next(ApiError.badRequest('Документ не найден'))
     }
+
+    // async getById(req, res, next) {
+    //     try {
+    //         console.log('URL: ', req.originalUrl)
+    //         const url = CLARIS_API_URL + '/vNext/v1/documents/' + req.params["id"]
+    //         let response = await fetch(url, getOptions(req.query.chat_id))
+    //         if (response.ok) {
+    //             console.log('status: OK')
+    //             const data = await response.json()
+    //             return res.status(200).json(data)
+    //         } else if (response.status === 401) {
+    //             console.log('status: 401')
+    //             const isNewToken = await getNewToken(req.query.chat_id)
+    //             if (isNewToken) {
+    //                 console.log('Повторная попытка выгрузки документа')
+    //                 let response = await fetch(url, getOptions(req.query.chat_id))
+    //                 if (response.ok) {
+    //                     const data = await response.json()
+    //                     return res.status(200).json(data)
+    //                 } else {
+    //                     return res.status(response.status).json({})
+    //                 }
+    //             } else {
+    //                 console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
+    //                 return res.status(500).json({message: 'Необходима авторизация в системе Кларис'})
+    //             }
+    //         } else {
+    //             console.log('status:', response.status, response.statusText)
+    //             return res.status(response.status).json({})
+    //         }
+    //
+    //     } catch (e) {
+    //         next(ApiError.badRequest(e.message))
+    //     }
+    // }
 
     async agree(req, res, next) {
         try {
