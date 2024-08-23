8af412296eb81ea7cd9bbd74dd78a1a93b2110d1
german.munshau <g.munshau@yandex.ru>
2024-08-23 12:34:52 +0600

    logging test

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -7,40 +7,32 @@ const CLARIS_API_URL = process.env.CLARIS_API_URL
 class DocumentController {
 
     async get(req, res, next) {
-        // console.log('URL: ', req.originalUrl)
-        logger.info(`DocumentController GET: ${req.originalUrl}`)
+        logger.info(`DocumentController get: ${req.originalUrl}`)
         const url = `${CLARIS_API_URL}/vNext/v1/documents?filterBy=serialNumber=${req.query.serialNumber}`
+        logger.info(`API GET: ${url}`)
         let response = await fetch(url, getOptions(req.query.chat_id))
         if (response.status === 200) {
             const data = await response.json()
             if (data.length === 0) {
-                logger.error('Документ не найден')
-                // console.log('status: 404 Документ не найден')
+                logger.error(`Документ № ${req.query.serialNumber} не найден`)
                 return next(ApiError.badRequest('Документ не найден'))
             } else {
-                // console.log('status: OK')
                 logger.info('OK')
                 return res.status(200).json(data[0])
             }
         } else if (response.status === 401) {
-            // console.log('status: 401 - Получение токена')
             logger.info('Получение токена')
             const isNewToken = await getNewToken(req.query.chat_id)
             if (isNewToken) {
-                // console.log('Повторная попытка выгрузки документа')
                 logger.info('Повторная попытка выгрузки документа')
                 let response = await fetch(url, getOptions(req.query.chat_id))
                 if (response.status === 200) {
-                    // console.log('status: OK')
                     logger.info('OK')
                     const data = await response.json()
-
                     if (data.length === 0) {
-                        // console.log('status: 404 Документ не найден')
-                        logger.error('Документ не найден')
+                        logger.error(`Документ № ${req.query.serialNumber} не найден`)
                         return next(ApiError.badRequest('Документ не найден'))
                     } else {
-                        // console.log('status: OK')
                         logger.info('OK')
                         return res.status(200).json(data[0])
                     }
@@ -48,69 +40,58 @@ class DocumentController {
                     return next(ApiError.common(response.status, 'Необходима авторизация в системе Кларис'))
                 }
             } else {
-                // console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
                 logger.error('Не найдено инфо о пользователе в базе бота, необходима авторизация')
-                // return next(ApiError.forbidden('Необходима авторизация в системе Кларис'))
                 return next(ApiError.common(response.status, 'Необходима авторизация в системе Кларис'))
             }
         }
-        // console.log('status: 404 Документ не найден')
-        logger.error('Документ не найден')
+        logger.error(`Документ № ${req.query.serialNumber} не найден`)
         return next(ApiError.badRequest('Документ не найден'))
     }
 
 
     async getById(req, res, next) {
-        // console.log('URL: ', req.originalUrl)
-        logger.info(`DocumentController GET_BY_ID: ${req.originalUrl}`)
+        logger.info(`DocumentController getById: ${req.originalUrl}`)
         const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}`
+        logger.info(`API GET: ${url}`)
         let response = await fetch(url, getOptions(req.query.chat_id))
         if (response.status === 200) {
             const data = await response.json()
-            // console.log('status: OK')
             logger.info('OK')
             return res.status(200).json(data)
         } else if (response.status === 401) {
-            // console.log('status: 401 - Получение токена')
             logger.info('Получение токена')
             const isNewToken = await getNewToken(req.query.chat_id)
             if (isNewToken) {
-                // console.log('Повторная попытка выгрузки документа')
                 logger.info('Повторная попытка выгрузки документа')
                 let response = await fetch(url, getOptions(req.query.chat_id))
                 if (response.status === 200) {
-                    // console.log('status: OK')
                     logger.info('OK')
                     const data = await response.json()
                     return res.status(200).json(data)
                 } else {
-                    // console.log('status: Необходима авторизация в системе Кларис')
                     logger.error('Необходима авторизация в системе Кларис')
                     return next(ApiError.forbidden('Необходима авторизация в системе Кларис'))
                 }
             } else {
-                // console.log('Не найдено инфо о пользователе в базе бота, необходима авторизация')
                 logger.error('Необходима авторизация в системе Кларис')
                 return next(ApiError.forbidden('Необходима авторизация в системе Кларис'))
             }
         }
-        // console.log('status: 404 Документ не найден')
         logger.error('Документ не найден')
         return next(ApiError.badRequest('Документ не найден'))
     }
 
     async agree(req, res, next) {
+        logger.info(`DocumentController agree: ${req.originalUrl}`)
         try {
             const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}/agree`
-            // console.log('URL:', url)
-            // console.log('body: ', req.body)
             logger.info(`URL: ${url}`)
             logger.info(`body: ${req.body}`)
             const {chatId, messageId, number, comment} = req.body
             const response = await fetch(url, postOptions(chatId, comment))
-            // console.log('Status:', response.status, response.statusText)
-            logger.info(`Status: ${response.status} ${response.statusText}`)
+            logger.info(`${response.status} ${response.statusText}`)
             if (response.status === 200) {
+                logger.info(`Документ № ${number} согласован`)
                 await bot.editMessageText(`Документ № ${number} согласован`, {
                     chat_id: chatId,
                     message_id: messageId
@@ -118,26 +99,27 @@ class DocumentController {
                 return res.status(response.status).json({})
             } else {
                 const errorMessage = `Ошибка при согласовании документа № ${number}`
+                logger.error(errorMessage)
                 await bot.sendMessage(chatId, errorMessage)
                 return next(ApiError.common(response.status, errorMessage))
             }
         } catch (e) {
+            logger.error(e.errorCode?.message)
             return next(ApiError.common(e.errorCode.message))
         }
     }
 
     async disagree(req, res, next) {
+        logger.info(`DocumentController disagree: ${req.originalUrl}`)
         try {
             const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}/disagree`
-            // console.log('URL:', url)
-            // console.log('body: ', req.body)
             logger.info(`URL: ${url}`)
             logger.info(`body: ${req.body}`)
             const {chatId, messageId, number, comment} = req.body
             const response = await fetch(url, postOptions(chatId, comment))
-            // console.log('Status:', response.status, response.statusText)
-            logger.info(`Status: ${response.status} ${response.statusText}`)
+            logger.info(`${response.status} ${response.statusText}`)
             if (response.status === 200) {
+                logger.info(`Документ № ${number} отклонен`)
                 await bot.editMessageText(`Документ № ${number} отклонен`, {
                     chat_id: chatId,
                     message_id: messageId
@@ -145,10 +127,12 @@ class DocumentController {
                 return res.status(response.status).json({})
             } else {
                 const errorMessage = `Ошибка при отклонении документа № ${number}`
+                logger.error(errorMessage)
                 await bot.sendMessage(chatId, errorMessage)
                 return next(ApiError.common(response.status, errorMessage))
             }
         } catch (e) {
+            logger.error(e.errorCode?.message)
             return next(ApiError.common(e.errorCode.message))
         }
     }
