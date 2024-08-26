e2f8020de34969b1f078c43101adea4b678652a8
german.munshau <g.munshau@yandex.ru>
2024-08-26 10:06:21 +0600

    refactoring

--- logger.js
+++ logger.js
@@ -9,11 +9,11 @@ const pino = require('pino')
 // logger.debug('debug');
 // logger.trace('trace');
 
-const getLogFileName = () => {
-    const date = new Date();
-    const res = date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + date.getDate()).slice(-2)
-    return res
-}
+// const getLogFileName = () => {
+//     const date = new Date();
+//     const res = date.getFullYear() + ('0' + (date.getMonth() + 1)).slice(-2) + ('0' + date.getDate()).slice(-2)
+//     return res
+// }
 
 
 const logger = pino({
@@ -24,7 +24,17 @@ const logger = pino({
                 target: 'pino-pretty',
                 options: {
                     colorize: false,
-                    destination: `${__dirname}/logs/app-${getLogFileName()}.log`,
+                    destination: `${__dirname}/logs/app-trace.log`,
+                    translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+                    ignore: "pid,hostname",
+                }
+            },
+            {
+                target: 'pino-pretty',
+                level: "error",
+                options: {
+                    colorize: false,
+                    destination: `${__dirname}/logs/app-error}.log`,
                     translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
                     ignore: "pid,hostname",
                 }
