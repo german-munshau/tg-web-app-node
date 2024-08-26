3a212b1df02b2153a2188b912e4c0963c84d45c4
german.munshau <g.munshau@yandex.ru>
2024-08-26 16:05:02 +0600

    refactoring

--- controllers/agreementHistoryController.js
+++ controllers/agreementHistoryController.js
@@ -7,21 +7,21 @@ const CLARIS_API_URL = process.env.CLARIS_API_URL
 class AgreementHistoryController {
 
 
+    // async get(req, res, next) {
+    //     logger.info(`AgreementHistoryController get: ${req.originalUrl}`)
+    //     const url = `${CLARIS_API_URL}/vNext/v1/agreementHistories?orderBy=date+desc,&filters=NoEmptyAgreed&filterBy=document.id="${req.params["id"]}"`
+    //     logger.info(`API GET: ${url}`)
+    //     const response = await getResponse(url, req.query.chat_id)
+    //     if (response.status === 200) {
+    //         return res.status(200).json(response.data)
+    //     } else {
+    //         logger.error(response.message)
+    //         return next(ApiError.common(response.status, response.message))
+    //     }
+    // }
+
+
     async get(req, res, next) {
-        logger.info(`AgreementHistoryController get: ${req.originalUrl}`)
-        const url = `${CLARIS_API_URL}/vNext/v1/agreementHistories?orderBy=date+desc,&filters=NoEmptyAgreed&filterBy=document.id="${req.params["id"]}"`
-        logger.info(`API GET: ${url}`)
-        const response = await getResponse(url, req.query.chat_id)
-        if (response.status === 200) {
-            return res.status(200).json(response.data)
-        } else {
-            logger.error(response.message)
-            return next(ApiError.common(response.status, response.message))
-        }
-    }
-
-
-    async getOld(req, res, next) {
         try {
             logger.info(`AgreementHistoryController get: ${req.originalUrl}`)
             const url = `${CLARIS_API_URL}/vNext/v1/agreementHistories?orderBy=date+desc,&filters=NoEmptyAgreed&filterBy=document.id="${req.params["id"]}"`
