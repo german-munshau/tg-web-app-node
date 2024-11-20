365b55cb4b525bd8055e6498a4659349d089adde
german.munshau <g.munshau@yandex.ru>
2024-11-20 11:38:45 +0600

    pay

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,6 +1,6 @@
 const ApiError = require('../error/ApiError');
 const logger = require('../logger')
-const {postOptions, getResponse, patchOptions} = require("../utils/api");
+const {postOptions, getResponse, patchOptions, getUserData} = require("../utils/api");
 
 const CLARIS_API_URL = process.env.CLARIS_API_URL
 
@@ -88,13 +88,19 @@ class DocumentController {
     async pay(req, res, next) {
         logger.info(`DocumentController pay: ${req.originalUrl}`)
         try {
-        const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}/rerouting`
+            const url = `${CLARIS_API_URL}/vNext/v1/documents/${req.params["id"]}/rerouting`
 
             const agreementId = '5079215165000' // маршрут 'Срочно оплатить'
             const kadrId = '4944011359000' // Святов
 
+
             logger.info(`URL: ${url} body: ${JSON.stringify(req.body)}`)
             const {chatId, messageId, number} = req.body
+
+            const userData = getUserData(chatId)
+
+            logger.info(`User Data: ${JSON.stringify(userData)}`)
+
             const response = await fetch(url, postOptions(chatId, {agreementId, kadrId}))
 
             logger.info(`Response: ${response?.status}`)
